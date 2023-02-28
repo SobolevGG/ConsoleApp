0b6bf4d7a07d4d627e43c7ffed8ddf5680c24d5c
sobol <sobol@SOBOLEVHOMEPC>
2023-03-01 03:49:43 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Model;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Name = "Ilya",
+                Surname = "Sobolev",
+                Age = 24
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Model
+{
+    public class Person
+    {
+        public string Name;
+
+        public string Surname;
+
+        public int Age;
+    }
+}
